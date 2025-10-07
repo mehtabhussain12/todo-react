fabdf8f5f4ee975dce9635a2129660d5ec2c47e3
mehtabhussain12 <mehtabmughal688@gmail.com>
2025-10-07 10:25:44 +0500

    Edit task complete

--- src/components/pages/crud/Todo.jsx
+++ src/components/pages/crud/Todo.jsx
@@ -1,55 +1,64 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Todo = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [editIndex, setEditIndex] = useState(null); 
 
-    const [tasks, setTasks] = useState([]);
-    const [newTask, setNewTask] = useState('');
+  const addOrUpdateTask = () => {
+    if (newTask.trim()) {
+      if (editIndex !== null) {
+        const updatedTasks = [...tasks];
+        updatedTasks[editIndex] = newTask;
+        setTasks(updatedTasks);
+        setEditIndex(null);
+      } else {
+        setTasks([...tasks, newTask]);
+      }
+      setNewTask(""); 
+    }
+  };
 
-    const addTask = () => {
-        if (newTask.trim()) {
-            setTasks([...tasks, newTask]);
-            setNewTask('');
-        }
-    };
-function deleteTask(index) {
-   let updatedTasks = [...tasks];  
-        updatedTasks.splice(index, 1); 
-        setTasks(updatedTasks);   
-}
-function editTask(index) {
-    let updateTasks = [...tasks]
-    let splice = updateTasks.splice(index, 1, prompt("Edit Task", updateTasks[index]))
-    setTasks(updateTasks)
-}
-    return (
-        <>
-            <div>
-                <h1>Todo List</h1>
-                <div>
-                    <input
-                        type="text"
-                        placeholder="Add a new task"
-                        value={newTask}
-                        onChange={(e) => setNewTask(e.target.value)}
-                    />
-                    <button onClick={addTask}>Add</button>
-                </div>
-                <ul>
-                    {tasks.map((el, index) => {
-                        return (
-                            <li key={index}>
-                                {el}
-                                <button onClick={() => deleteTask(index)}>D</button>
-                                  <button onClick={() => editTask(index)}>Edit</button>
-                            </li>
-                        );
-                    })}
-                </ul>
-        
-            </div>
+  const deleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+   
+  };
 
-        </>
-    )
-}
+  const editTask = (index) => {
+    setNewTask(tasks[index]); 
+    setEditIndex(index);
+  };
 
-export default Todo
+  return (
+    <>
+      <div style={{ margin: "20px" }}>
+        <h1>Todo List</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Add or edit a task"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <button onClick={addOrUpdateTask}>
+            {editIndex !== null ? "Update" : "Add"}
+          </button>
+        </div>
+
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task}
+              <button onClick={() => deleteTask(index)}>D</button>
+              <button onClick={() => editTask(index)}>E</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
